f9f3569f99898e97b148a293ab1b51cf255849ae
Bikki Mahato <bikkimahato123@gmail.com>
2021-08-28 23:48:28 +0530

    Count Occurrences Of Anagrams | Sliding Window [Todo]

---
+++ coding-problems/src/sliding_window/CountOccurrencesOfAnagrams.java
@@ -0,0 +1,16 @@
+package sliding_window;
+
+public class CountOccurrencesOfAnagrams {
+
+	public static void main(String[] args) {
+		String text = "forxxorfxdofr";
+		String pattern = "for";
+		System.out.println(solve(text, pattern));
+	}
+
+	private static int solve(String text, String pattern) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
