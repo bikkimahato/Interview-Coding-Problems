f802128ee7d5d260c1deb38dc2fcb28ec37ac68a
Bikki Mahato <bikkimahato123@gmail.com>
2022-01-14 20:02:41 +0530

    Create PartitionIntoSubsets.java

---
+++ coding-problems/src/dp/PartitionIntoSubsets.java
@@ -0,0 +1,19 @@
+//Partition Into Subsets
+//You are required to print the number of ways in which these elements can be partitioned in k non-empty subsets.
+package dp;
+
+public class PartitionIntoSubsets {
+
+	public static void main(String[] args) throws Exception {
+		int n = 4;
+		int k = 3;
+
+		long res = partitionKSubset(n, k);
+		System.out.println(res);
+	}
+
+	public static long partitionKSubset(int n, int k) {
+		// write your code here
+	}
+
+}
