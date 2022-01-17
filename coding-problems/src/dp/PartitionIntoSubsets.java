ba109e22e9cc3ecb5f0dd711d3d858dcf3e7742b
Bikki Mahato <bikkimahato123@gmail.com>
2022-01-17 22:53:11 +0530

    Partition into Subsets | Dynamic Programming

--- coding-problems/src/dp/PartitionIntoSubsets.java
+++ coding-problems/src/dp/PartitionIntoSubsets.java
@@ -13,7 +13,19 @@ public class PartitionIntoSubsets {
 	}
 
 	public static long partitionKSubset(int n, int k) {
-		// write your code here
+		long dp[][] = new long[k + 1][n + 1];
+		for (int i = 1; i <= k; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (j < i) {
+					dp[i][j] = 0;
+				} else if (j == i) {
+					dp[i][j] = 1;
+				} else {
+					dp[i][j] = i * dp[i][j - 1] + dp[i - 1][j - 1];
+				}
+			}
+		}
+		return dp[k][n];
 	}
 
 }
