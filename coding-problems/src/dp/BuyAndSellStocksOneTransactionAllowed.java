20a0fc961a0a601003601a4f1182f3b0b881d801
Bikki Mahato <bikkimahato123@gmail.com>
2022-01-18 19:13:17 +0530

    Buy And Sell Stocks - One Transaction Allowed | Dynamic Programming

--- coding-problems/src/dp/BuyAndSellStocksOneTransactionAllowed.java
+++ coding-problems/src/dp/BuyAndSellStocksOneTransactionAllowed.java
@@ -5,8 +5,26 @@ package dp;
 public class BuyAndSellStocksOneTransactionAllowed {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n = 9;
+		int prices[] = { 11, 6, 7, 19, 4, 1, 6, 18, 4 };
+		System.out.println(calculateMaxProfit(n, prices));
+	}
 
+	private static int calculateMaxProfit(int n, int[] arr) {
+		int least = Integer.MAX_VALUE;
+		int totalProfit = 0;
+		int profitToday = 0;
+
+		for (int i = 0; i < n; i++) {
+			if (arr[i] < least) {
+				least = arr[i];
+			}
+			profitToday = arr[i] - least;
+			if (profitToday > totalProfit) {
+				totalProfit = profitToday;
+			}
+		}
+		return totalProfit;
 	}
 
 }
