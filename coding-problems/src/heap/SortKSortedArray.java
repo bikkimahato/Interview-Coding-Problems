2243db6f501332a35813c74a506e554ff85d5350
Bikki Mahato <bikkimahato123@gmail.com>
2022-02-04 22:27:15 +0530

    Sort K-sorted Array

---
+++ coding-problems/src/heap/SortKSortedArray.java
@@ -0,0 +1,28 @@
+//Sort K-sorted Array
+package heap;
+
+import java.util.PriorityQueue;
+
+public class SortKSortedArray {
+	public static void main(String[] args) {
+		int arr[] = { 2, 3, 1, 4, 5, 6, 7, 8, 9 };
+		int k = 3;
+		sortKSorted(arr, k);
+	}
+
+	private static void sortKSorted(int[] arr, int k) {
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+		for (int i = 0; i <= k; i++) {
+			minHeap.add(arr[i]);
+		}
+
+		for (int i = k + 1; i < arr.length; i++) {
+			System.out.println(minHeap.poll());
+			minHeap.add(arr[i]);
+		}
+		while (minHeap.size() > 0) {
+			System.out.println(minHeap.poll());
+		}
+
+	}
+}
