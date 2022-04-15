bae20b095305be98a2e83d096ac7ff499008fc44
Bikki Mahato <bikkimahato123@gmail.com>
2022-04-15 21:56:52 +0530

    K Reverse In Linked List

--- coding-problems/src/linked_list/KReverse.java
+++ coding-problems/src/linked_list/KReverse.java
@@ -332,7 +332,36 @@ public class KReverse {
 		}
 
 		public void kReverse(int k) {
-			// write your code here
+			LinkedList prev = null;
+			while (this.size > 0) {
+				LinkedList curr = new LinkedList();
+
+				if (this.size >= k) {
+					for (int i = 0; i < k; i++) {
+						int val = this.getFirst();
+						this.removeFirst();
+						curr.addFirst(val);
+					}
+				} else {
+					int s = this.size();
+					for (int i = 0; i < s; i++) {
+						int val = this.getFirst();
+						this.removeFirst();
+						curr.addLast(val);
+					}
+				}
+
+				if (prev == null) {
+					prev = curr;
+				} else {
+					prev.tail.next = curr.head;
+					prev.tail = curr.tail;
+					prev.size += curr.size;
+				}
+			}
+			this.head = prev.head;
+			this.tail = prev.tail;
+			this.size = prev.size;
 		}
 	}
 
